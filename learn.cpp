92bc9d0e3f72bf88949a9a24b60fedc6d30853e7
elaa06dance <elaa06@ioiio.cc>
2025-10-21 14:13:46 +0700

    pair1

--- learn.cpp
+++ learn.cpp
@@ -2,30 +2,31 @@
 #include <iostream>
 
 template <typename T>
-struct Pair
+struct Pair0
+{
+    T first;
+    T second;
+};
+
+template <typename T>
+struct Pair1
 {
     T first{};
     T second{};
 };
 
-// template <typename T>
-// struct Pair
-// {
-//     T first;
-//     T second;
-// };
-
 int main()
 {
     std::cout << "learn" << std::endl;
 
-    Pair<int> p0;
+    Pair0<int> p0;
     std::cout << p0.first << ' ' << p0.second << '\n';
 
-    // Pair<int> p1{5, 6}; // instantiates Pair<int> and creates object p1
-    // std::cout << p1.first << ' ' << p1.second << '\n';
+    // Pair1 compiles error for C++11
+    Pair1<int> p1{5, 6}; // instantiates Pair<int> and creates object p1
+    std::cout << p1.first << ' ' << p1.second << '\n';
 
-    // Pair<double> p2{1.2, 3.4}; // instantiates Pair<double> and creates object p2
+    // Pair1<double> p2{1.2, 3.4}; // instantiates Pair<double> and creates object p2
     // std::cout << p2.first << ' ' << p2.second << '\n';
 
     return 0;
